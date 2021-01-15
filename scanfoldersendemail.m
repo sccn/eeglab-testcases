f7d2b72e39df7be1173a99caca0b52a367493931
Arnaud Delorme <arnodelorme@gmail.com>
2021-01-15 08:36:06 -1000

    only send error larger than a specific size

--- scanfoldersendemail.m
+++ scanfoldersendemail.m
@@ -2,8 +2,10 @@ folder = dir(sprintf('~/nemar/unittestresults/%s/*.txt', datestr(now, 'YY_mm_dd'
 
 if ~isempty(folder)
 	for iFile = 1:length(folder)
-		system([ 'mail -n -s "' folder(iFile).name ' generated errors" arno@ucsd.edu < ' fullfile(folder(iFile).folder, folder(iFile).name) ]);
+        if folder(iFile).bytes > 150
+            system([ 'mail -n -s "' folder(iFile).name ' generated errors" arno@ucsd.edu < ' fullfile(folder(iFile).folder, folder(iFile).name) ]);
+        end
 	end
 else
-        system([ 'echo "Do not forget to change revision number in Content.m" | mail -n -s " no errors - ready to compile" arno@ucsd.edu' ]);
+    % system([ 'echo "Do not forget to change revision number in Content.m" | mail -n -s " no errors - ready to compile" arno@ucsd.edu' ]);
 end
