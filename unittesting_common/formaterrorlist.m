32aabe65b82736c1a54899cc750c4e36d2ddc53a
Arnaud Delorme <arnodelorme@gmail.com>
2021-01-25 14:56:00 -1000

    fixing not being able to write file

--- unittesting_common/formaterrorlist.m
+++ unittesting_common/formaterrorlist.m
@@ -10,29 +10,37 @@ end
 
 fid  = fopen(fileName, 'w');
 
-% write octave info
-if ~ismatlab
-    fprintf(fid, 'Runnning Octave!\n');
-    fprintf(fid, '----------------\n');
+if fid ~= -1
+    % write octave info
+    if ~ismatlab
+        fprintf(fid, 'Runnning Octave!\n');
+        fprintf(fid, '----------------\n');
+    end
+    
+    % write option file
+    % -----------------
+    eeglab_options;
+    options = { 'option_single' 'option_memmapdata' 'option_eegobject' 'option_computeica' 'option_donotusetoolboxes' };
+    for ind =1:length(options)
+        var = eval(options{ind});
+        fprintf(fid, '%s = %d\n', options{ind}, var);
+    end
+    
+    % write errors
+    % ------------
+    for index = 1:length(error_list)
+        fprintf(fid, '*********************************************\n');
+        fprintf(fid, 'function %s\n', error_list(index).function);
+        fprintf(fid, '   msg %s\n'  , error_list(index).errmsg.message);
+        fprintf('function %s\n', error_list(index).function);
+        fprintf('   msg %s\n'  , error_list(index).errmsg.message);
+        fprintf('*********************************************\n');
+    end
+    fclose(fid);
 end
 
-% write option file
-% -----------------
-eeglab_options;
-options = { 'option_single' 'option_memmapdata' 'option_eegobject' 'option_computeica' 'option_donotusetoolboxes' };
-for ind =1:length(options)
-    var = eval(options{ind});
-    fprintf(fid, '%s = %d\n', options{ind}, var);    
-end
-
-% write errors
-% ------------
 for index = 1:length(error_list)
-    fprintf(fid, '*********************************************\n');
-    fprintf(fid, 'function %s\n', error_list(index).function);
-    fprintf(fid, '   msg %s\n'  , error_list(index).errmsg.message);
     fprintf('function %s\n', error_list(index).function);
     fprintf('   msg %s\n'  , error_list(index).errmsg.message);
     fprintf('*********************************************\n');
 end
-fclose(fid);
